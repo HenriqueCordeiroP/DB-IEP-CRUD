38b617ed1465cc9a03f5fad410a0600db6954e8e
Henrique Cordeiro <h.cordeirop@gmail.com>
2023-11-03 15:06:00 -0300

    navbar and patient template, missing table

---
+++ iep/src/main/java/com/db_project/iep/ComponentsController.java
@@ -0,0 +1,15 @@
+package com.db_project.iep;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/components")
+public class ComponentsController {
+	
+	@GetMapping("/navbar")
+	public String loadNavbar() {
+		return "components/navbar";
+	}
+}
--- iep/src/main/java/com/db_project/iep/IepApplication.java
+++ iep/src/main/java/com/db_project/iep/IepApplication.java
@@ -2,12 +2,19 @@ package com.db_project.iep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class IepApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IepApplication.class, args);
 	}
-
+	
+	@GetMapping("/")
+	public String greet() {
+		return "";
+	}
 }
