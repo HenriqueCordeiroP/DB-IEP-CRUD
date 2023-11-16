1dfb907d2ce2f687e06f9c7a46938b9dbb964fcf
dudasfig <112442570+dudasfig@users.noreply.github.com>
2023-11-16 18:36:43 -0300

    controller, service de doctr + edit

--- iep/src/main/java/com/db_project/iep/Controllers/DoctorController.java
+++ iep/src/main/java/com/db_project/iep/Controllers/DoctorController.java
@@ -1,11 +1,20 @@
 package com.db_project.iep.Controllers;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.db_project.iep.Service.DoctorService;
+import com.db_project.iep.Service.PatientService;
+
+import Utils.Conversion;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/doctor")
@@ -20,12 +29,52 @@ public class DoctorController {
 
 	@GetMapping("/read")
 	public String read(Model model){
+		List<Map<String, Object>> doctors = doctorService.getDoctorList();
+		model.addAttribute("doctors", doctors);
 		return "doctor/read";
 	}
-}
+
 
 @GetMapping("/create")
 	public String create() {
 		return "doctor/create";
 	}
- 
+
+	@PostMapping("/create")
+	public String create_form(HttpServletRequest request) {
+		String name = Conversion.parseStringOrNull(request.getParameter("nome"));
+		String rg = Conversion.parseStringOrNull(request.getParameter("rg"));
+		String cpf = Conversion.parseStringOrNull(request.getParameter("cpf"));
+		String celular = Conversion.parseStringOrNull(request.getParameter("celular"));
+		String residencial = Conversion.parseStringOrNull(request.getParameter("residencial"));
+		String email = Conversion.parseStringOrNull(request.getParameter("email"));
+		String cidade = Conversion.parseStringOrNull(request.getParameter("cidade"));
+		String bairro = Conversion.parseStringOrNull(request.getParameter("bairro"));
+		String rua = Conversion.parseStringOrNull(request.getParameter("rua"));
+		String numero = Conversion.parseStringOrNull(request.getParameter("numero"));
+		String dt_nascimento = Conversion.parseStringOrNull(request.getParameter("data_nascimento"));
+		String sexo = Conversion.parseStringOrNull(request.getParameter("sexo"));
+		String crm = Conversion.parseStringOrNull(request.getParameter("crm"));
+		String rqe = Conversion.parseStringOrNull(request.getParameter("rqe"));
+		String especialidade =Conversion.parseStringOrNull( request.getParameter("especialidade"));
+		
+		int patientResult = doctorService.createDoctor(name, cpf, rg, celular, residencial, email, cidade, bairro, rua, numero, 
+				dt_nascimento, sexo, crm, rqe, especialidade);
+		if (patientResult > 0) {
+			return "redirect:/doctor/read";			
+		} else {
+			// return error
+			return null;
+		}
+	}
+	
+	@GetMapping("/delete/{cpf}")
+	public String delete(@PathVariable String cpf) {
+		int patientResult = doctorService.deleteDoctor(cpf);
+		if (patientResult > 0) {
+			return "redirect:/doctor/read";			
+		}
+		return null;
+	}
+
+}
--- iep/src/main/java/com/db_project/iep/Service/DoctorService.java
+++ iep/src/main/java/com/db_project/iep/Service/DoctorService.java
@@ -1,12 +1,13 @@
 package com.db_project.iep.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-
 @Service
 public class DoctorService {
 	
@@ -15,6 +16,54 @@ public class DoctorService {
 	@Autowired
 	public DoctorService(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
+	}
+	
+	public List<Map<String, Object>> getDoctorList(){
+		return jdbcTemplate.queryForList("SELECT pe.nome, e.email, pe.tel_celular, m.nome_especialidade,pe.cpf"  
+		+"FROM medico m"
+		+"JOIN pessoa pe ON m.cpf_pessoa = pe.cpf "
+		+"JOIN email e ON m.cpf_pessoa = e.cpf_pessoa "
+		+"GROUP BY pe.nome, e.email, pe.tel_celular , m.nome_especialidade "
+		+"ORDER BY nome ASC");
+	}
+
+	public int createDoctor(String name, String cpf, String rg, String celular,String residencial,String email,String cidade, String bairro, String rua, String numero, String dt_nascimento, String sexo, String crm, String rqe, String especialidade) {
+		PessoaService pessoaService = new PessoaService(jdbcTemplate);
+		int pessoaResult = pessoaService.createPessoa(cpf, rg, name, dt_nascimento, sexo, residencial, celular, cidade, bairro, rua, numero);
+		int emailResult = pessoaService.createEmail(cpf, email);
+		int doctorResult = 0;
+		if (pessoaResult > 0 && emailResult > 0) {
+			String sql = "INSERT INTO medico VALUES(?, ?, ?, ?, ?)";
+			return jdbcTemplate.update(sql, crm, rqe, especialidade, cpf);
+		}
+		return doctorResult;
+	}
+
+	public int updateDoctor(String name, String cpf, String rg, String celular,String residencial,String email,
+							  String cidade, String bairro, String rua, String numero, String dt_nascimento, String sexo,String crm, String rqe, String especialidade) {
+		PessoaService pessoaService = new PessoaService(jdbcTemplate);
+		int pessoaResult = pessoaService.updatePessoa(cpf, rg, name, dt_nascimento, sexo, residencial, celular, cidade, bairro, rua, numero);
+		pessoaService.createEmail(cpf, email);
+		int doctorResult = 0;
+		if (pessoaResult > 0) {
+			String sql = "UPDATE medico "
+			+ "SET crm = ?, rqe = ?, nome_especialidade = ?"
+			+ "WHERE cpf_pessoa = ?";
+			doctorResult = jdbcTemplate.update(sql, crm, rqe, especialidade, cpf);
+		}
+		return doctorResult;
+	}
+
+	public int deleteDoctor(String cpf) {
+		PessoaService pessoaService = new PessoaService(jdbcTemplate);
+		int emailResult = pessoaService.deleteEmail(cpf); 
+		String sql = "DELETE FROM medico m WHERE p.cpf_pessoa = ?";
+		int doctorResult = jdbcTemplate.update(sql, cpf);
+		if (doctorResult > 0 && emailResult > 0) {
+			return pessoaService.deletePessoa(cpf);
+		}
+		
+		return 0;
 	}
 
 }
